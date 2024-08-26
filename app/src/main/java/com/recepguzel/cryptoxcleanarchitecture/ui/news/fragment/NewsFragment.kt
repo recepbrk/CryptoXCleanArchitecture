9882e7d8b9c129640044d1171a1e3fee845ccaea
Recep Güzel <recepgzelr@gmail.com>
2024-08-26 16:09:55 +0300

    Interstitial Admob add <3

--- app/src/main/java/com/recepguzel/cryptoxcleanarchitecture/ui/news/fragment/NewsFragment.kt
+++ app/src/main/java/com/recepguzel/cryptoxcleanarchitecture/ui/news/fragment/NewsFragment.kt
@@ -10,6 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.recepguzel.cryptoxcleanarchitecture.data.model.NewsResponse
 import com.recepguzel.cryptoxcleanarchitecture.databinding.FragmentNewsBinding
 import com.recepguzel.cryptoxcleanarchitecture.ui.news.adapter.NewsAdapter
 import com.recepguzel.cryptoxcleanarchitecture.ui.news.viewmodel.NewsViewModel
@@ -22,8 +29,8 @@ class NewsFragment : Fragment() {
     private lateinit var binding: FragmentNewsBinding
     private val newsViewModel: NewsViewModel by viewModels()
     private lateinit var newsAdapter: NewsAdapter
-
-    // SafeArgs to receive the category argument
+    private var mInterstitialAd: InterstitialAd? = null
+    private final val TAG = "NewsFragment"
     private val args: NewsFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -38,14 +45,12 @@ class NewsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val veri = arguments?.getString("name")
-        Log.d("NewsFragment", "name = $veri")
-
         val category = args.category // Get the category from SafeArgs
         Log.d("NewsFragment", "onViewCreated: Category received: $category")
 
         setupRecyclerView()
         observeData()
+        loadInterAds()
 
         category?.let {
             newsViewModel.getCryptoNews(it)
@@ -56,6 +61,39 @@ class NewsFragment : Fragment() {
         newsAdapter = NewsAdapter()
         binding.newsRecyclerview.adapter = newsAdapter
         Log.d("NewsFragment", "setupRecyclerView: RecyclerView initialized")
+
+        newsAdapter.OnItemClickListener { article ->
+            if (mInterstitialAd != null) {
+                mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                    override fun onAdDismissedFullScreenContent() {
+                        navigateToNewsDetail(article)
+                    }
+
+                    override fun onAdFailedToShowFullScreenContent(adError: AdError) {
+                        navigateToNewsDetail(article)
+                    }
+                }
+                mInterstitialAd?.show(requireActivity())
+            } else {
+                navigateToNewsDetail(article)
+            }
+        }
+    }
+
+    private fun loadInterAds() {
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(requireContext(), "ca-app-pub-3940256099942544/1033173712", adRequest, object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, "Ad failed to load: ${adError.message}")
+                mInterstitialAd = null
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mInterstitialAd = interstitialAd
+            }
+        })
     }
 
     private fun observeData() {
@@ -65,13 +103,6 @@ class NewsFragment : Fragment() {
                     Log.d("NewsFragment", "observeData: Data loaded successfully")
                     newsAdapter.differ.submitList(resource.data?.articles)
                     binding.newsProgress.visibility = View.GONE
-
-                    newsAdapter.OnItemClickListener { article ->
-                        Log.d("NewsFragment", "observeData: Item clicked - Navigating to details")
-                        val action =
-                            NewsFragmentDirections.actionNewsFragmentToNewsDetailFragment(article)
-                        findNavController().navigate(action)
-                    }
                 }
 
                 is Resource.Loading -> {
@@ -86,5 +117,11 @@ class NewsFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun navigateToNewsDetail(article: NewsResponse.Article) {
+        Log.d("NewsFragment", "navigateToNewsDetail: Navigating to details")
+        val action = NewsFragmentDirections.actionNewsFragmentToNewsDetailFragment(article)
+        findNavController().navigate(action)
     }
 }
